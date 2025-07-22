8af402be8d4b1e38245c86f340ce5023258d6d92
kurumi <15281351767@163.com>
2025-07-22 22:07:32 +0800

    feat: MouseLeaveEvent 事件 feat: FrameEvent 事件

---
+++ include/event/frame/FrameCreateEvent.h
@@ -0,0 +1,10 @@
+#ifndef FRAMECREATE_H
+#define FRAMECREATE_H
+
+#include "Event.h"
+
+class FrameCreateEvent: public Event {
+
+};
+
+#endif
---
+++ include/event/frame/FrameEvent.h
@@ -0,0 +1,10 @@
+#ifndef FRAMEEVENT_H
+#define FRAMEEVENT_H
+
+#include "Event.h"
+
+class FrameEvent: public Event {
+
+};
+
+#endif
